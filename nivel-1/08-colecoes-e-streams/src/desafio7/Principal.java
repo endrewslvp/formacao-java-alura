92683e333692e92b7b3e3fea4c3a66cdad31a560
Endrew <endrewslvp@gmail.com>
2026-05-21 19:22:12 -0300

    feat(08-colecoes-e-streams): adiciona desafio 7

---
+++ nivel-1/08-colecoes-e-streams/src/desafio7/Principal.java
@@ -0,0 +1,27 @@
+package desafio7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Imagine que a equipe de RH precisa gerar relatórios específicos com listas de funcionários cujos nomes tenham um
+ * determinado número de caracteres ou menos para facilitar segmentações internas. Para isso, você deve ajudá-los
+ * desenvolvendo um programa capaz de:
+ *
+ * Criar uma lista contendo os nomes dos funcionários.
+ * Filtrar apenas os nomes que tenham menos de 5 caracteres ou exatamente 5 caracteres.
+ * Criar uma lista contendo os nomes filtrados.
+ * Exibir a lista filtrada no console.
+ */
+
+public class Principal {
+    public static void main(String[] args) {
+        List<String> funcionarios = List.of("Ana", "Bruno", "Carlos", "Amanda", "Alice", "Daniel", "Caroline");
+        List<String> funcionariosNomesCurtos = funcionarios.stream()
+                .filter(f -> f.length()<=5)
+                .collect(Collectors.toUnmodifiableList());
+        System.out.println(funcionariosNomesCurtos);
+
+    }
+}
